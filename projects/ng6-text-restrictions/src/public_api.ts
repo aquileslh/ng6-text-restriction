08e7c171172c7eed705e0ce5005e10bef1fe9780
aquileslh <aquileslazaroh@sistemashumanos.com>
2018-12-28 16:13:33 -0600

    Exportando la libreria para que pueda ser accesada

--- projects/ng6-text-restrictions/src/public_api.ts
+++ projects/ng6-text-restrictions/src/public_api.ts
@@ -1,7 +1,9 @@
 /*
  * Public API Surface of ng6-text-restrictions
- */
+ *
 
 export * from './lib/ng6-text-restrictions.service';
 export * from './lib/ng6-text-restrictions.component';
 export * from './lib/ng6-text-restrictions.module';
+*/
+export * from './lib/alpha-mx/alpha-mx.directive';
