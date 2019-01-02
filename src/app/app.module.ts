285f00fbcbb5ce2490ae4cb4f0f4ab632ed20465
aquileslh <aquileslazaroh@sistemashumanos.com>
2019-01-02 12:45:32 -0600

    Importación de módulo Ng6TextRestrictionsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Ng6TextRestrictionsModule } from 'ng6-text-restrictions';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng6TextRestrictionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
