e10f1fe0e7b5039d110e9cc0ff5cf620d5cd7a69
aquileslh <aquileslazaroh@sistemashumanos.com>
2019-01-03 16:18:26 -0600

    Modificación de funciones para validacion de caracteres abecedario español

--- projects/ng6-text-restrictions/src/lib/alpha-mx/alpha-mx.directive.ts
+++ projects/ng6-text-restrictions/src/lib/alpha-mx/alpha-mx.directive.ts
@@ -7,7 +7,10 @@ import { NgControl } from '@angular/forms';
 })
 export class AlphaMxDirective {
 
+  /** Expreción regular que se evalua */
   public ALPHA_MX = '^([A-Za-záéíóúñüÁÉÍÓÚÑÜ ])+$';
+  /**  Nuevo objeto RegExp */
+  regExp = new RegExp(this.ALPHA_MX);
 
   constructor(
     private ngControl: NgControl
@@ -15,26 +18,45 @@ export class AlphaMxDirective {
 
   /**
    * Maneja evento de cambio en el input y se ejecuta la funcion onInputChange.
-   * @param {*} event Valor de input
+   * {*} event Valor de input
    * @memberof AlphaMxDirective
    */
   @HostListener('ngModelChange', ['$event'])
   onModelChange(event) {
     this.onInputChange(event, false);
+    console.log(event);
   }
 
+  /**
+   * Maneja el evento de tecla de retroceso enviando true como segundo parametro.
+   * {*} event Evento de tecla de retroceso
+   * @memberof AlphaMxDirective
+   */
   @HostListener('keydown.backspace', ['$event'])
   keydownBackspace(event) {
     this.onInputChange(event.target.value, true);
   }
 
+  /**
+   * Evalua los parametros con RegExp y actualiza el contenido del input
+   * por medio de ngControl.valueAccessor, si el valor no es valido se remplaza
+   * el contenido con la funcion replace.
+   * {*} event Valor que se obtiene del input.
+   * {*} backSpace Bandera valor true si el evento es desatado por esta tecla.
+   * @memberof AlphaMxDirective
+   */
   onInputChange(event, backSpace) {
-    const regExp = new RegExp(this.ALPHA_MX);
-    if ((regExp.test(event))) {
-      this.ngControl.valueAccessor.writeValue(event);
-    } else {
-      const newVal = event.substring(0, event.length - 1);
+    let newVal: string;
+    if (backSpace) {
+      newVal = event;
       this.ngControl.valueAccessor.writeValue(newVal);
+    } else {
+      if ((this.regExp.test(event))) {
+        this.ngControl.valueAccessor.writeValue(event);
+      } else {
+        newVal = event.replace(/[^A-Za-záéíóúñüÁÉÍÓÚÑÜ\s]/g, '');
+        this.ngControl.valueAccessor.writeValue(newVal);
+      }
     }
   }
 
